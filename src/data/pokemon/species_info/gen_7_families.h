c940b27c392b755de818d9d89671cca9d68a9f62
Eemeliri <eemeli.rintanen@gmail.com>
2026-08-22 10:42:33 +0300

    add title defense round number to HoF screen and fix some pokedex totem forms being displayed

--- src/data/pokemon/species_info/gen_7_families.h
+++ src/data/pokemon/species_info/gen_7_families.h
@@ -5906,8 +5906,7 @@ const struct SpeciesInfo gSpeciesInfoGen7[] =
         )
         .levelUpLearnset = sHakamoOLevelUpLearnset,
         .teachableLearnset = sHakamoOTeachableLearnset,
-        .evolutions = EVOLUTION({EVO_LEVEL, 45, SPECIES_KOMMO_O},
-                                {EVO_NONE, 0, SPECIES_KOMMO_O_TOTEM}),
+        .evolutions = EVOLUTION({EVO_LEVEL, 45, SPECIES_KOMMO_O}),
     },
 
     [SPECIES_KOMMO_O] =
